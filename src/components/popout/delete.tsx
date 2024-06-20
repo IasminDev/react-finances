83e3dab73164aa730d81d026ce07cf77e1d5c12d
IasminDev <iasmin.albr@gmail.com>
2024-06-20 09:32:05 -0300

    first crud

--- src/components/popout/delete.tsx
+++ src/components/popout/delete.tsx
@@ -1,27 +1,61 @@
 import { ComponentProps, Dispatch, SetStateAction } from "react";
 import { Button } from "../ui/button";
 import { twMerge } from "tailwind-merge";
+import { api } from "../../lib/server";
 
-interface DeleteProps extends ComponentProps<"div">{
-    openDelete?: boolean;
-    goals?: boolean;
-    setOpenDeleteProps: Dispatch<SetStateAction<boolean>>;
+interface DeleteProps extends ComponentProps<"div"> {
+  openDelete?: boolean;
+  goals?: boolean;
+  savings?: boolean;
+  revenueId?: number;
+  userId?: string;
+  userToken?: string;
+  setOpenDeleteProps: Dispatch<SetStateAction<boolean>>;
 }
 
+export function Delete({
+  openDelete,
+  goals,
+  savings,
+  revenueId,
+  userId,
+  userToken,
+  setOpenDeleteProps,
+}: DeleteProps) {
+  const handleDelete = () => {
+    if (savings) {
+      api
+        .delete(`/${userId}/revenues/${revenueId}`, {
+          headers: { token: `Bearer ${userToken}` },
+        })
+        .then(() => {
+          console.log(`Revenue with ID ${revenueId} deleted successfully`);
+          setOpenDeleteProps(false);
+        })
+        .catch((error) => {
+          console.error("Error deleting revenue:", error);
+        });
+    } else {
+      console.error("Missing userId or revenueId");
+    }
+  };
 
-export function Delete({ openDelete, goals, setOpenDeleteProps }: DeleteProps){
-    return(
-        <div className={twMerge(
-            `fixed inset-0 flex items-center justify-center z-50`,
-            openDelete ? "flex" : "hidden"
-        )}>
-            <div className="w-80 h-50 bg-slate-700 rounded-md border border-white/10 p-4 flex flex-col items-center justify-between"> 
-                <p className="mb-4 text-center">Are you sure you want to delete this {goals ? "goal" : "transaction"}?</p>
-                <div className="flex space-x-2">
-                    <Button>Confirm</Button>
-                    <Button onClick={() => setOpenDeleteProps(false)}>Cancel</Button>
-                </div>
-            </div>
+  return (
+    <div
+      className={twMerge(
+        `fixed inset-0 flex items-center justify-center z-50`,
+        openDelete ? "flex" : "hidden"
+      )}
+    >
+      <div className="w-80 h-50 bg-slate-700 rounded-md border border-white/10 p-4 flex flex-col items-center justify-between">
+        <p className="mb-4 text-center">
+          Are you sure you want to delete this {goals ? "goal" : "transaction"}?
+        </p>
+        <div className="flex space-x-2">
+          <Button onClick={handleDelete}>Confirm</Button>
+          <Button onClick={() => setOpenDeleteProps(false)}>Cancel</Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/financial-planning/financial-goals.tsx
+++ src/pages/financial-planning/financial-goals.tsx
@@ -11,27 +11,21 @@ import { TableHeader } from "../../components/ui/table/table-header";
 import { TableRow } from "../../components/ui/table/table-row";
 import { TableCell } from "../../components/ui/table/table-cell";
 
-import {
-  PencilIcon,
-  Trash2Icon,
-} from "lucide-react";
-
+import { PencilIcon, Trash2Icon } from "lucide-react";
 
 import { Delete } from "../../components/popout/delete";
 import { Edit } from "../../components/popout/edit";
 import { useNavigate } from "react-router-dom";
 
-interface Goal {
+export interface Goal {
   description: string;
   value: number;
   date: string;
 }
 
-
 export function FinancialGoals() {
-
-  const user = localStorage.getItem("user")
-  const navigate = useNavigate()
+  const user = localStorage.getItem("user");
+  const navigate = useNavigate();
 
   const [goals, setGoals] = useState<Goal[]>([]);
   const [description, setDescription] = useState<string>("");
@@ -42,67 +36,64 @@ export function FinancialGoals() {
   const [infoDate, setInfoDate] = useState<string>("");
   const [modal, setModal] = useState<boolean>(false);
   const [modalIndex, setModalIndex] = useState<number | null>(null);
-  const[openDelete, setOpenDelete] = useState<boolean>(false);
-  const[openEdit, setOpenEdit] = useState<boolean>(false);
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
+  const [openEdit, setOpenEdit] = useState<boolean>(false);
 
   const handleInsert = () => {
-   if(user){
-     if (!description) {
-       setInfoDesc("Please enter a description");
-       return;
-     } else {
-       setInfoDesc("");
-     }
-     if (typeof value !== "number" || value <= 0 || isNaN(value)) {
-       setInfoValue("Please enter a valid value greater than 0");
-       return;
-     } else {
-       setInfoValue("");
-     }
-     if (!date) {
-       setInfoDate("Please select a date");
-       return;
-     } else {
-       setInfoDate("");
-     }
- 
-     const newGoal: Goal = {
-       description,
-       value,
-       date,
-     };
- 
-     setGoals([...goals, newGoal]);
-     setDescription("");
-     setValue("");
-     setDate("");
-     setInfoDesc("");
-     setInfoValue("");
-   }
-   else{
-    navigate("/log-in-account")
-   }
+    if (user) {
+      if (!description) {
+        setInfoDesc("Please enter a description");
+        return;
+      } else {
+        setInfoDesc("");
+      }
+      if (typeof value !== "number" || value <= 0 || isNaN(value)) {
+        setInfoValue("Please enter a valid value greater than 0");
+        return;
+      } else {
+        setInfoValue("");
+      }
+      if (!date) {
+        setInfoDate("Please select a date");
+        return;
+      } else {
+        setInfoDate("");
+      }
+
+      const newGoal: Goal = {
+        description,
+        value,
+        date,
+      };
+
+      setGoals([...goals, newGoal]);
+      setDescription("");
+      setValue("");
+      setDate("");
+      setInfoDesc("");
+      setInfoValue("");
+    } else {
+      navigate("/log-in-account");
+    }
   };
 
-  const handleOpenDelete = (index : number) => {
-    if(user){
+  const handleOpenDelete = (index: number) => {
+    if (user) {
       setOpenDelete(true);
       setModal(true);
       setModalIndex(index);
+    } else {
+      navigate("/log-in-account");
     }
-   else{
-    navigate("/log-in-account")
-   }
   };
 
-  const handleOpenEdit = (index : number) => {
-    if(user){
+  const handleOpenEdit = (index: number) => {
+    if (user) {
       setOpenEdit(true);
       setModal(true);
       setModalIndex(index);
-    }
-    else{
-     navigate("/log-in-account")
+    } else {
+      navigate("/log-in-account");
     }
   };
 
@@ -179,87 +170,110 @@ export function FinancialGoals() {
           </tr>
         </thead>
         <tbody>
-        
-        {goals.map((goal, index) => (
-          <TableRow key={index} className="flex flex-col p-2 sm:hidden">
-            <div className="flex justify-between">
-              <TableHeader>Goal</TableHeader>
+          {goals.map((goal, index) => (
+            <TableRow key={index} className="flex flex-col p-2 sm:hidden">
+              <div className="flex justify-between">
+                <TableHeader>Goal</TableHeader>
+                <TableCell>{goal.description}</TableCell>
+              </div>
+              <div className="flex justify-center">
+                <hr className="w-60 p-1 border-white/10" />
+              </div>
+              <div className="flex justify-between">
+                <TableHeader>Value</TableHeader>
+                <TableCell>$ {goal.value.toFixed(2)}</TableCell>
+              </div>
+              <div className="flex justify-center">
+                <hr className="w-60 p-1 border-white/10" />
+              </div>
+              <div className="flex justify-between">
+                <TableHeader>Date</TableHeader>
+                <TableCell>{dayjs(goal.date).format("MM/DD/YYYY")}</TableCell>
+              </div>
+              <div className="flex justify-center">
+                <hr className="w-60 p-1 border-white/10" />
+              </div>
+              <TableCell className="flex gap-3 items-center">
+                <IconButton>
+                  <PencilIcon
+                    onClick={() => handleOpenEdit(index)}
+                    className="size-4"
+                  />
+                </IconButton>
+                {modal && modalIndex === index && (
+                  <Edit
+                    goals={true}
+                    openEdit={openEdit}
+                    goal={goals[modalIndex] || undefined}
+                    setOpenEditProps={setOpenEdit}
+                  />
+                )}
+                <IconButton>
+                  <Trash2Icon
+                    onClick={() => handleOpenDelete(index)}
+                    className="size-4"
+                  />
+                </IconButton>
+                {modal && modalIndex === index && (
+                  <Delete
+                    goals={true}
+                    openDelete={openDelete}
+                    setOpenDeleteProps={setOpenDelete}
+                  />
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+          {goals.map((goal, index) => (
+            <TableRow key={index} className="hidden sm:table-row">
               <TableCell>{goal.description}</TableCell>
-            </div>
-            <div className="flex justify-center">
-              <hr className="w-60 p-1 border-white/10" />
-            </div>
-            <div className="flex justify-between">
-              <TableHeader>Value</TableHeader>
               <TableCell>$ {goal.value.toFixed(2)}</TableCell>
-            </div>
-            <div className="flex justify-center">
-              <hr className="w-60 p-1 border-white/10" />
-            </div>
-            <div className="flex justify-between">
-              <TableHeader>Date</TableHeader>
               <TableCell>{dayjs(goal.date).format("MM/DD/YYYY")}</TableCell>
-            </div>
-            <div className="flex justify-center">
-              <hr className="w-60 p-1 border-white/10" />
-            </div>
-            <TableCell className="flex gap-3 items-center">
-            <IconButton>
-                <PencilIcon onClick={() => handleOpenEdit(index)} className="size-4" />
-            </IconButton>
-            {modal && modalIndex === index && (
-                <Edit goals={true} openEdit={openEdit} setOpenEditProps={setOpenEdit}/>
-              )}
-            <IconButton>
-                <Trash2Icon onClick={() => handleOpenDelete(index)} className="size-4" />
-            </IconButton>
-              {modal && modalIndex === index && (
-                <Delete goals={true} openDelete={openDelete} setOpenDeleteProps={setOpenDelete}/>
-              )}
-            </TableCell>
-          </TableRow>
-            ))}
-        {goals.map((goal, index) => (
-          <TableRow key={index} className="hidden sm:table-row">
-            <TableCell>{goal.description}</TableCell>
-            <TableCell>$ {goal.value.toFixed(2)}</TableCell>
-            <TableCell>{dayjs(goal.date).format("MM/DD/YYYY")}</TableCell>
-            <TableCell className="flex gap-3 items-center">
-            <IconButton>
-                <PencilIcon onClick={() => handleOpenEdit(index)} className="size-4" />
-            </IconButton>
-            {modal && modalIndex === index && (
-                <Edit goals={true} openEdit={openEdit} setOpenEditProps={setOpenEdit}/>
-              )}
-            <IconButton>
-                <Trash2Icon onClick={() => handleOpenDelete(index)} className="size-4" />
-            </IconButton>
-              {modal && modalIndex === index && (
-                <Delete openDelete={openDelete} setOpenDeleteProps={setOpenDelete}/>
-              )}
-            </TableCell>
-          </TableRow>
-            ))}
-
-      </tbody>
+              <TableCell className="flex gap-3 items-center">
+                <IconButton>
+                  <PencilIcon
+                    onClick={() => handleOpenEdit(index)}
+                    className="size-4"
+                  />
+                </IconButton>
+                {modal && modalIndex === index && (
+                  <Edit
+                    goals={true}
+                    openEdit={openEdit}
+                    goal={goals[modalIndex] || undefined}
+                    setOpenEditProps={setOpenEdit}
+                  />
+                )}
+                <IconButton>
+                  <Trash2Icon
+                    onClick={() => handleOpenDelete(index)}
+                    className="size-4"
+                  />
+                </IconButton>
+                {modal && modalIndex === index && (
+                  <Delete
+                    openDelete={openDelete}
+                    setOpenDeleteProps={setOpenDelete}
+                  />
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </tbody>
         <tfoot>
-         <tr className="flex flex-col sm:table-row">
+          <tr className="flex flex-col sm:table-row">
             <TableCell colSpan={5} className="text-center">
               <span>Showing {goals.length} transactions</span>
             </TableCell>
           </tr>
         </tfoot>
       </Table>
-        {openEdit && (
-        <div
-        className='fixed z-40 inset-0 bg-slate-950/70 cursor-pointer'
-        />
-        )}
+      {openEdit && (
+        <div className="fixed z-40 inset-0 bg-slate-950/70 cursor-pointer" />
+      )}
       {openDelete && (
-        <div
-        className='fixed z-40 inset-0 bg-slate-950/70 cursor-pointer'
-        />
-        )}
+        <div className="fixed z-40 inset-0 bg-slate-950/70 cursor-pointer" />
+      )}
     </div>
   );
 }
